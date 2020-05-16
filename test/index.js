7095b0c73027fbcf3a65a98dbce81e6005d675f4
Niklas Ekman <niklas@stockfiller.com>
2020-05-16 20:13:35 +0200

    no special treatment for functions

--- test/index.js
+++ test/index.js
@@ -46,10 +46,4 @@ describe('empty-js', function () {
     assert(empty(0) == true)
     assert(empty(42) == false)
   })
-
-  it('Testing functions', function () {
-    assert(empty(function(){}) == true)
-    assert(empty(function(a,b){}) == false)
-  })
-
 })
