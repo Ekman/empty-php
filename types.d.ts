0c82ad305eef7c4cc018e4eb7c7510db0f130bbd
Niklas Ekman <nikl.ekman@gmail.com>
2020-11-18 18:14:31 +0100

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -4,4 +4,4 @@
  * @returns true if the value is empty, false otherwise
  * @see https://www.php.net/manual/en/function.empty.php
  */
-export default function empty<T = unknown>(value: NonNullable<T> | undefined | null): value is null | undefined;
+export default function empty<T = unknown>(value: NonNullable<T> | undefined | null): value is undefined;
