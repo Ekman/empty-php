ff389fda314ba7ce2924dab21b9296bdc431d2b4
Niklas Ekman <nikl.ekman@gmail.com>
2020-12-18 16:50:54 +0100

    Evolve TS definitions by adding function for each data type (#8)
    * Evolve TS definitions by adding function for each data type
    
    * update version

--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,87 @@
+type Undefined = undefined | null;
+
+/**
+ * The definition of an empty boolean in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type EmptyBoolean = Undefined | false;
+
+/**
+ * The definition of an empty string in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type EmptyString = Undefined | "" | "0";
+
+/**
+ * The definition of an empty number in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type EmptyNumber = Undefined | 0 | 0.0;
+
+/**
+ * The definition of an empty array in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type EmptyArray = Undefined | [];
+
+/**
+ * The definition of an empty object in PHP
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+export type EmptyObject = Undefined | {};
+
 /**
  * The definition of an empty value in PHP
  * @see https://www.php.net/manual/en/function.empty.php
  */
-export type Empty = undefined | null | "" | 0 | 0.0 | "0" | false | [] | {};
+export type Empty = Undefined | EmptyBoolean | EmptyString | EmptyNumber | EmptyArray | EmptyObject;
 
 /**
- * Determines if the input value is empty
+ * Determines a boolean is empty
  * @param value The value to determine if empty
  * @returns true if the value is empty, false otherwise
  * @see https://www.php.net/manual/en/function.empty.php
  */
-export default function empty(value: unknown): value is Empty;
+declare function empty(value?: boolean | Undefined): value is EmptyBoolean;
+
+/**
+ * Determines a string is empty
+ * @param value The value to determine if empty
+ * @returns true if the value is empty, false otherwise
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+declare function empty(value?: string | Undefined): value is EmptyString;
+
+/**
+ * Determines a number is empty
+ * @param value The value to determine if empty
+ * @returns true if the value is empty, false otherwise
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+declare function empty(value?: number | Undefined): value is EmptyNumber;
+
+/**
+ * Determines an array is empty
+ * @param value The value to determine if empty
+ * @returns true if the value is empty, false otherwise
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+declare function empty(value?: unknown[] | Undefined): value is EmptyArray;
+
+/**
+ * Determines an object is empty
+ * @param value The value to determine if empty
+ * @returns true if the value is empty, false otherwise
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+declare function empty(value?: object | Undefined): value is EmptyObject;
+
+/**
+ * Determines an unknown value is empty
+ * @param value The value to determine if empty
+ * @returns true if the value is empty, false otherwise
+ * @see https://www.php.net/manual/en/function.empty.php
+ */
+declare function empty(value?: unknown): value is Empty;
+
+export default empty;
