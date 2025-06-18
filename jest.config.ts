80dbe4fa29229ee1d608787f1325fed1685e11bf
Niklas Ekman <nikl.ekman@gmail.com>
2025-06-18 20:16:45 +0200

    Minify configurations

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,6 @@ import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
-    roots: ['<rootDir>/src'],
-    extensionsToTreatAsEsm: [".ts"],
-    transform: {
-        "\\.ts$": ["ts-jest", {useESM: true}],
-    },
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-    }
 }
 
 export default config;
